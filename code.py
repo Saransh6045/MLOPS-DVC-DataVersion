0c4ff9ba96b9edef448ec8efe19dba147ec7fa12
Saransh Varshney <varshneysaransh6045@gmail.com>
2026-09-07 12:43:14 +0530

    Sample data added

---
+++ code.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import os
+
+#Creating a sample dataframe through pandas library
+data = {
+    'Name' : ['Saransh', 'Rajat', 'Kanika'],
+    'Age' : [21, 23, 20],
+    'State' : ['Uttar Pradesh', 'Rajasthan', 'Maharashtra']
+}
+
+df = pd.DataFrame(data)
+
+#Ensuring the data directory exists at the root level
+data_dir = 'data'
+os.makedirs(data_dir, exist_ok=True)
+
+#Define the file path
+file_path = os.path.join(data_dir, 'sample_data.csv')
+
+#Saving the dataframe to a csv file 
+df.to_csv(file_path, index=False)
+
+print("Data added successfully")
